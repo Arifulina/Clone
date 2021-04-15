d20bac08cad7e7a35e1beb37d4b4ad21e9d7092d
Екатерина <e.arifulina@gmail.com>
2021-04-15 09:12:51 +0300

    Добавлена задача по вкладу

--- js/script.js
+++ js/script.js
@@ -71,16 +71,22 @@ else {
 
 let vklad = +(prompt("Введите сумму вклада:"));
 let percent = +(prompt("Введите процент:"));
+
 let result = (((vklad * percent * 1) / 100) + vklad);
-alert("Размер вклада 1 год " + result.toFixed(0) + " рублей");
+    alert("Размер вклада 1 год " + result.toFixed(0) + " рублей");
+
 let result2 = (((result * percent * 1) / 100) + result);
-alert("Размер вклада 2 год " + result2.toFixed(0) + " рублей");
+    alert("Размер вклада 2 год " + result2.toFixed(0) + " рублей");
+
 let result3 = (((result2 * percent * 1) / 100) + result2);
-alert("Размер вклада 3 год " + result3.toFixed(0) + " рублей");
+    alert("Размер вклада 3 год " + result3.toFixed(0) + " рублей");
+
 let result4 = (((result3 * percent * 1) / 100) + result3);
-alert("Размер вклада 4 год " + result4.toFixed(0) + " рублей");
+    alert("Размер вклада 4 год " + result4.toFixed(0) + " рублей");
+
 let result5 = (((result4 * percent * 1) / 100) + result4);
-alert("Размер вклада 5 год " + result5.toFixed(0) + " рублей");
+    alert("Размер вклада 5 год " + result5.toFixed(0) + " рублей");
+
 
 /**** 4.Запросить у пользователя ввод числа и сохранить это число в переменную a.Если
 переменная a равна 10, то выведите 'Верно', иначе выведите 'Неверно'.****/
