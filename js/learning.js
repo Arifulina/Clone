78f25b5c8946363835053cac3ef9db41950355ae
Екатерина <e.arifulina@gmail.com>
2021-05-13 17:39:59 +0300

    Пример позиционирования и спрайтов

--- js/learning.js
+++ js/learning.js
@@ -1,204 +1,222 @@
+// const time = new Date().getHours();
+// const dayTheme = document.getElementById("dayTheme");
+// const nightTheme = document.getElementById("nightTheme");
+
+// if(time <= 20 && time >= 6) {
+// 	dayTheme.remove();
+// }
+// else {
+// 	nightTheme.remove();
+// }
+
 //форма с переключателями
 const form = document.getElementById("generateCard");
 //коллекция переключателей
 const radioButtons = document.querySelectorAll("[type='radio']");
 
 let card; //объект - создаваемая карточка
-const text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur temporibus maxime doloremque minima? Odio quaerat, et at necessitatibus error animi, impedit libero sit, expedita nulla debitis. Quasi qui ipsa commodi.";
+const text =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur temporibus maxime doloremque minima? Odio quaerat, et at necessitatibus error animi, impedit libero sit, expedita nulla debitis. Quasi qui ipsa commodi.";
 let imgUrl = ""; //адрес изображения
 
 class Card {
-    //конструктор с параметрами, создающий объект карточку (не DOM-объект)
+  //конструктор с параметрами, создающий объект карточку (не DOM-объект)
 
-    _id;
-    _className;
-    _text;
-    _imageSource;##
-    width = 100;#
-    height = 50;
+  _id;
+  _className;
+  _text;
+  _imageSource;
+  #width = 100;
+  #height = 50;
 
-    get Width() {
-        return this.#width;
+  get Width() {
+    return this.#width;
+  }
+
+  set Width(value) {
+    if (document.getElementById("card")) {
+      this.#width = value;
+    } else {
+      this.#width = 0;
     }
+  }
 
-    set Width(value) {
-        if (document.getElementById("card")) {
-            this.#width = value;
-        } else {
-            this.#width = 0;
-        }
+  get Height() {
+    return this.#height;
+  }
+
+  set Height(value) {
+    if (document.getElementById("card")) {
+      this.#height = value;
+    } else {
+      this.#height = 0;
     }
+  }
 
-    get Height() {
-        return this.#height;
+  constructor(id, className, text, imgUrl) {
+    this.Id = id;
+    this.ClassName = className;
+    this.Text = text;
+    this.ImageSource = imgUrl;
+  }
+
+  get Id() {
+    return this._id;
+  }
+
+  set Id(value) {
+    this._id = value;
+  }
+
+  get ClassName() {
+    return this._className;
+  }
+
+  set ClassName(value) {
+    this._className = value;
+  }
+
+  get Text() {
+    return this._text;
+  }
+
+  set Text(value) {
+    this._text = value;
+  }
+
+  get ImageSource() {
+    return this._imageSource;
+  }
+
+  set ImageSource(value) {
+    this._imageSource = value;
+  }
+
+  //метод - создание карточки
+  createCard() {
+    let card; //объект - блок (div)
+    let p; //объект - абзац (p)
+
+    card = document.createElement("div");
+    card.id = this.Id;
+    card.className = "card";
+    card.classList.add(this.ClassName);
+
+    form.insertAdjacentElement("afterend", card); //добавляем карточку после формы
+
+    p = document.createElement("p");
+    p.textContent = this.Text; //добавляем текст абзацу
+
+    card.append(p); //добавляем абзац в конец карточки
+
+    //если путь к картинке задан, то создаём её
+    if (typeof this.ImageSource != "undefined") {
+      this.createImage(card);
     }
+  }
 
-    set Height(value) {
-        if (document.getElementById("card")) {
-            this.#height = value;
-        } else {
-            this.#height = 0;
-        }
+  //метод - создание изображения в карточке
+  createImage(card) {
+    let img; //объект - изображение в карточке
+
+    img = document.createElement("img");
+    img.src = this.ImageSource;
+    img.alt = "Фото";
+
+    card.prepend(img); //добавляем изображение в начало карточки
+  }
+
+  //метод - модификация объекта (карточки)
+  modifiedCard(typeCard, imgUrl) {
+    this.ClassName = typeCard;
+    this.ImageSource = imgUrl;
+
+    //удаление изображения в случае выбора "Без фото"
+    if (typeCard == "card-no-photo") {
+      document.querySelector(".card img").remove();
+    } else {
+      if (!document.querySelector(".card img")) {
+        this.createImage(document.getElementById("card"));
+      }
     }
+    document.getElementById("card").className = `card ${this.ClassName}`;
+  }
 
-    constructor(id, className, text, imgUrl) {
-        this.Id = id;
-        this.ClassName = className;
-        this.Text = text;
-        this.ImageSource = imgUrl;
-    }
-
-    get Id() {
-        return this._id;
-    }
-
-    set Id(value) {
-        this._id = value;
-    }
-
-    get ClassName() {
-        return this._className;
-    }
-
-    set ClassName(value) {
-        this._className = value;
-    }
-
-    get Text() {
-        return this._text;
-    }
-
-    set Text(value) {
-        this._text = value;
-    }
-
-    get ImageSource() {
-        return this._imageSource;
-    }
-
-    set ImageSource(value) {
-        this._imageSource = value;
-    }
-
-    //метод - создание карточки
-    createCard() {
-        let card; //объект - блок (div)
-        let p; //объект - абзац (p)
-
-        card = document.createElement("div");
-        card.id = this.Id;
-        card.className = "card";
-        card.classList.add(this.ClassName);
-
-        form.insertAdjacentElement("afterend", card); //добавляем карточку после формы
-
-        p = document.createElement("p");
-        p.textContent = this.Text; //добавляем текст абзацу
-
-        card.append(p); //добавляем абзац в конец карточки
-
-        //если путь к картинке задан, то создаём её
-        if (typeof this.ImageSource != "undefined") {
-            this.createImage(card);
-        }
-    }
-
-    //метод - создание изображения в карточке
-    createImage(card) {
-        let img; //объект - изображение в карточке
-
-        img = document.createElement("img");
-        img.src = this.ImageSource;
-        img.alt = "Фото";
-
-        card.prepend(img); //добавляем изображение в начало карточки
-    }
-
-    //метод - модификация объекта (карточки)
-    modifiedCard(typeCard, imgUrl) {
-        this.ClassName = typeCard;
-        this.ImageSource = imgUrl;
-
-        //удаление изображения в случае выбора "Без фото"
-        if (typeCard == "card-no-photo") {
-            document.querySelector(".card img").remove();
-        } else {
-            if (!document.querySelector(".card img")) {
-                this.createImage(document.getElementById("card"));
-            }
-        }
-        document.getElementById("card").className = `card ${this.ClassName}`;
-    }
-
-    static getSizeCard(id) {
-        return [document.getElementById(id).offsetWidth, document.getElementById(id).offsetHeight];
-    }
+  static getSizeCard(id) {
+    return [
+      document.getElementById(id).offsetWidth,
+      document.getElementById(id).offsetHeight,
+    ];
+  }
 }
 
-class ChildCard extends Card {#
-    cardName;
+class ChildCard extends Card {
+  #cardName;
 
-    constructor(id, className, text, imgUrl) {
-        super(id, className, text, imgUrl);
-        this.CardName = "Дочерняя";
-    }
+  constructor(id, className, text, imgUrl) {
+    super(id, className, text, imgUrl);
+    this.CardName = "Дочерняя";
+  }
 
-    get CardName() {
-        return this.#cardName;
-    }
+  get CardName() {
+    return this.#cardName;
+  }
 
-    set CardName(value) {
-        this.#cardName = value;
-    }
+  set CardName(value) {
+    this.#cardName = value;
+  }
 
-    modifiedCard(typeCard) {
-        if (document.getElementById("child")) {
-            this.ClassName = typeCard;
-            document.getElementById("child").classList.add(this.ClassName);
-            document.querySelector(".card img").style.width = "75%";
-            document.querySelector(".card p").style.width = "80%";
-            document.querySelector(".card p").style.margin = "10px auto";
-        }
+  modifiedCard(typeCard) {
+    if (document.getElementById("child")) {
+      this.ClassName = typeCard;
+      document.getElementById("child").classList.add(this.ClassName);
+      document.querySelector(".card img").style.width = "75%";
+      document.querySelector(".card p").style.width = "80%";
+      document.querySelector(".card p").style.margin = "10px auto";
     }
+  }
 }
 
-let child = new ChildCard("child", "card-decorative", text, "https://picsum.photos/500/300");
+let child = new ChildCard(
+  "child",
+  "card-decorative",
+  text,
+  "https://picsum.photos/500/300"
+);
 child.createCard();
 child.modifiedCard("card-circle");
-console.log(document.querySelector(".card img") instanceof HTMLImageElement);
-
+// console.log(document.querySelector(".card img") instanceof HTMLImageElement);
 
 for (let radio of radioButtons) {
-    radio.addEventListener("change", () => {
-        if (radio.value == "card-no-photo") {
-            imgUrl = undefined;
+  radio.addEventListener("change", () => {
+    if (radio.value == "card-no-photo") {
+      imgUrl = undefined;
+    } else {
+      imgUrl = "https://picsum.photos/500/300";
+    }
+
+    if (document.getElementById("card")) {
+      card.modifiedCard(radio.value, imgUrl);
+    } else {
+      card = new Card("card", radio.value, text, imgUrl);
+      card.createCard();
+    }
+
+    if (card) {
+      const p = document.getElementById("cardSize");
+      const span = document.querySelector(".card-size span");
+      p.classList.remove("hide");
+
+      for (let key in Card.getSizeCard("card")) {
+        if (key == 0) {
+          span.textContent = `ширина: ${Card.getSizeCard("card")[key]}`;
         } else {
-            imgUrl = "https://picsum.photos/500/300";
+          span.textContent += `, высота: ${Card.getSizeCard("card")[key]}`;
         }
-
-        if (document.getElementById("card")) {
-            card.modifiedCard(radio.value, imgUrl);
-        } else {
-            card = new Card("card", radio.value, text, imgUrl);
-            card.createCard();
-        }
-
-        if (card) {
-            const p = document.getElementById("cardSize");
-            const span = document.querySelector(".card-size span");
-            p.classList.remove("hide");
-
-            for (let key in Card.getSizeCard("card")) {
-                if (key == 0) {
-                    span.textContent = `ширина: ${Card.getSizeCard("card")[key]}`;
-                } else {
-                    span.textContent += `, высота: ${Card.getSizeCard("card")[key]}`;
-                }
-            }
-        }
-    });
+      }
+    }
+  });
 }
-
 
 // const input = {
 //   type: "number",
@@ -228,7 +246,6 @@ for (let radio of radioButtons) {
 // const inputAge = createElem("input");
 // document.body.insertAdjacentElement("afterbegin", inputAge);
 // inputAge.focus();
-
 
 // let p; //абзац с результатом
 // let error; //окно с ошибкой
@@ -312,7 +329,7 @@ for (let radio of radioButtons) {
 //   else if(years % 10 >= 2
 //            && years % 10 <= 4
 //            && (years % 100 > 14 || years % 100 < 12)
-//          ) 
+//          )
 //   {
 //     result = "года";
 //   }
@@ -370,8 +387,6 @@ for (let radio of radioButtons) {
 //   }
 // }
 
-
-
 // *** Функциональное выражение (Function Expression) ***
 // let number = 5;
 // const factorial = function inner(number) {
@@ -381,8 +396,6 @@ for (let radio of radioButtons) {
 //   return number * inner(number - 1);
 // }
 // console.log(`Факториал числа ${number} = ${factorial(number)}`);
-
-
 
 //let arr = new Map();
 //let arr = new Map([
@@ -398,13 +411,13 @@ for (let radio of radioButtons) {
 
 //for (let key of arr.keys()) {}
 //for (let value of arr.values()) {}
-//for (let elem of arr) { 
+//for (let elem of arr) {
 //    elem[0] //key
 //    elem[1] //value
 //  }
 
 //arr.forEach((value, key) => {
-// 
+//
 // })
 
 //*** Ассоциативный массив (с помощью конструктора Map) ***
@@ -439,28 +452,6 @@ for (let radio of radioButtons) {
 //   console.log("Ключ: " + key);
 //   console.log("Значение: " + value);
 // })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Разобраться с Reduce!!!
 // mas = [-2, -4, 5, 20, 5, 2, 7];
