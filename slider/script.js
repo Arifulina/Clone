5fc2772079f2fa05160dd5e0eaef48e490701933
Екатерина <e.arifulina@gmail.com>
2021-05-27 11:05:21 +0300

    Slider

---
+++ slider/script.js
@@ -0,0 +1,80 @@
+const slider = document.querySelector(".slider");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const slides = document.querySelectorAll(".slide");
+const slideIcons = document.querySelectorAll(".slide-icon");
+const numberOfSlides = slides.length;
+let slideNumber = 0;
+
+// ползунок изображения следующая кнопка
+
+nextBtn.addEventListener("click", () => {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+  slideIcons.forEach((slideIcon) => {
+    slideIcon.classList.remove("active");
+  });
+
+  slideNumber++;
+
+  if (slideNumber > numberOfSlides - 1) {
+    slideNumber = 0;
+  }
+
+  slides[slideNumber].classList.add("active");
+  slideIcons[slideNumber].classList.add("active");
+});
+
+// предыдущая кнопка слайдера изображения
+prevBtn.addEventListener("click", () => {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+  slideIcons.forEach((slideIcon) => {
+    slideIcon.classList.remove("active");
+  });
+
+  slideNumber--;
+
+  if (slideNumber < 0) {
+    slideNumber = numberOfSlides - 1;
+  }
+
+  slides[slideNumber].classList.add("active");
+  slideIcons[slideNumber].classList.add("active");
+});
+
+// автозапуск слайдера изображения
+let playSlider;
+
+let repeater = () => {
+  playSlider = setInterval(function () {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+    slideIcons.forEach((slideIcon) => {
+      slideIcon.classList.remove("active");
+    });
+
+    slideNumber++;
+
+    if (slideNumber > numberOfSlides - 1) {
+      slideNumber = 0;
+    }
+
+    slides[slideNumber].classList.add("active");
+    slideIcons[slideNumber].classList.add("active");
+  }, 4000);
+};
+repeater();
+
+// останавливаем автовоспроизведение слайдера изображения при наведении курсора мыши
+slider.addEventListener("mouseover", () => {
+  clearInterval(playSlider);
+});
+
+// снова запускаем автозапуск слайдера изображения при наведении курсора мыши
+slider.addEventListener("mouseout", () => {
+  repeater();
+});
